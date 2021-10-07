d6988dda50427d1ad003ce076fedbb1fcdc8ee0d
Md. Sujon Parvez <sujon.parvez@datasoft-bd.com>
2021-10-07 17:52:23 +0600

    tttt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
+import Navbar from './components/layouts/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import EditUser from './components/users/EditUser';
+import Users from './components/pages/Users';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch >
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About}/>
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/users/edit/:id" component={EditUser} />              
+          <Route exact path="/users" component={Users} />        
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
